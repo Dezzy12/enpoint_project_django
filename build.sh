60357fcfd93dee4cc0468a1b1ad2ec933a9b7390
Dezzy12 <desmondneba134@gmail.com>
2023-09-07 14:00:21 +0100

    deployment scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-source tf-env/bin/activate
-
-pip install django
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
